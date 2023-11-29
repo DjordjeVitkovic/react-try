57a70984fafe84b1ef84f66dae597c874257a048
Djordje <djordjev9@gmail.com>
2023-11-29 20:00:54 +0100

    Start movieLand app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import {useEffect, useState} from "react";
+import React from "react";
+import { useEffect } from "react";
 import './App.css';
+import SearchIcon from './search.svg';
 
+const API_URL = 'http://www.omdbapi.com?apikey=6c112dd0'
 const App = () => {
-    const [counter, setCounter] = useState(0);
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
 
     useEffect(() => {
-        // alert("Changed counter to:" + counter)
-        setCounter(100);
-    }, []);
+        searchMovies('Spiderman')
+    },[]);
 
     return (
-        <div className="App">
-            <br/>
-            <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
-            <h1>{counter}</h1>
-            <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+        <div className="app">
+            <h1>MovieLand</h1>
+            <div className="search">
+                <input placeholder="Search for movies" value="Superman" onChange={() => {}}/>
+                <img src={SearchIcon} alt="search"/>
+            </div>
+
         </div>
     );
 }
